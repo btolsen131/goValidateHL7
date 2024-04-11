bf45c2c02d122a6841e93bff8f229c0aa1870cf5
btolsen131 <btolsen131@gmail.com>
2024-04-10 22:29:10 -0400

    implemented structs for server and message

--- main.go
+++ main.go
@@ -5,11 +5,13 @@ import (
   "encoding/json"
   "os"
   "net"
+  "log"
   "strconv"
 )
 
 type Config struct {
   Port int 
+  SendAcks bool
   Fields map[string]string
 }
 func LoadConfigs() (*Config, error){
@@ -25,7 +27,78 @@ func LoadConfigs() (*Config, error){
   }
 
   return &config, nil
-  
+}
+
+type Message struct {
+  sender net.Conn
+  body []byte
+}
+
+type Server struct {
+  listenPort string
+  sendAcks bool
+  listener net.Listener
+  quitChan chan struct{}
+  msgChan chan Message 
+}
+
+func NewServer(port string, ack bool) *Server {
+  return &Server{
+    listenPort: port,
+    sendAcks: ack,
+    quitChan: make(chan struct{}),
+    msgChan: make(chan Message, 10),
+  }
+}
+
+func (s *Server) Start () error {
+  listener, err := net.Listen("tcp", s.listenPort)
+  if err != nil {
+    return err
+  }
+  defer listener.Close()
+  s.listener = listener
+
+  go s.acceptLoop()
+
+  <-s.quitChan
+  close(s.msgChan)
+
+  return nil
+}
+
+func (s *Server) acceptLoop() {
+  for {
+    conn, err := s.listener.Accept()
+    if err != nil {
+      fmt.Printf("Error accepting the connection %s \n", err)
+      continue
+    }
+    fmt.Printf("New Connection made. %s\n", conn.RemoteAddr())
+    go s.readLoop(conn)
+  }
+}
+
+func (s *Server) readLoop(conn net.Conn) {
+  defer conn.Close()
+  buf := make([]byte, 2048)
+  for {
+    n, err := conn.Read(buf)
+    if err != nil {
+      fmt.Printf("Error reading from connection to buffer %s", err)
+      continue
+    }
+    s.msgChan <- Message {
+      sender: conn,
+      body:buf[:n],
+    }
+
+    //Ack feature here? 
+    if(s.sendAcks){
+      conn.Write([]byte("ack"))
+    }
+
+  }
 }
 
 
@@ -38,13 +111,16 @@ func main() {
   }
   
   //Open Listener
-  listener, err := net.Listen("tcp", ":"+strconv.Itoa(config.Port))
-  if err != nil {
-    fmt.Printf("Error setting up lister: %s\n", err)
-    os.Exit(1)
-  }
-  defer listener.Close()
-  fmt.Printf("HL7 Validator started and listening on port %n\n", config.Port)
+  server := NewServer(":" + strconv.Itoa(config.Port),config.SendAcks)
+  log.Fatal(server.Start())
+
+  fmt.Printf("HL7 Validator started and listening on port %s\n", strconv.Itoa(config.Port))
+
+  go func(){
+    for msg := range server.msgChan{
+      fmt.Printf("Received message %s \n \n ", string(msg.body))
+    }
+  }()
 
 
   //Validate messages based on configurations
